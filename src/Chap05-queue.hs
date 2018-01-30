0fca625aa6b33f8abd214a410c4ace6d39647629
Zhihong Cheng <chengzh2008@gmail.com>
2018-01-29 21:32:10 -0800

    implement double queue with two lists

--- src/Chap05-queue.hs
+++ src/Chap05-queue.hs
@@ -53,6 +53,63 @@ someBQ = do
   sample $ (arbitrary :: Gen (BatchedQueue Int))
   sample $ (arbitrary :: Gen (BatchedQueue String))
 
+
+-- Deque class
+-- exercise 5.1 implementation of double deque
+class Queue q => Deque q where
+  cons :: a -> q a -> q a
+  lastq :: q a -> a
+  initq :: q a -> q a
+
+data DoubleEndQueue a = DQ [a] [a] deriving Show
+
+checkfDQ :: DoubleEndQueue a -> DoubleEndQueue a
+checkfDQ dq@(DQ f r)
+  | null f = DQ (reverse $ take halfR r) (drop halfR r)
+  | null r = DQ (drop halfF f) (reverse $ take halfF f)
+  | otherwise = dq
+  where halfR = length r `div` 2
+        halfF = length f `div` 2
+
+instance Queue DoubleEndQueue where
+  empty = DQ [] []
+
+  isEmpty (DQ [] []) = True
+  isEmpty (DQ _ _) = False
+
+  snoc (DQ f r) a = checkfDQ $ (DQ f (a:r))
+
+  headq dq@(DQ f r)
+    | isEmpty dq = error "empty deque"
+    | null f = last r
+    | null r = head f
+    | otherwise = head f
+
+  tailq dq@(DQ f r)
+    | isEmpty dq = error "empty deque"
+    | null f = checkfDQ $ DQ f (init r)
+    | null r = checkfDQ $ DQ (tail f) r
+    | otherwise = checkfDQ $ DQ (tail f) r
+
+instance Deque DoubleEndQueue where
+  cons a dq@(DQ f r) = checkfDQ $ DQ (a:f) r
+
+  lastq dq@(DQ f r)
+    | isEmpty dq = error "empty deque"
+    | null f = head r
+    | null r = last f
+    | otherwise = last f
+
+  initq dq@(DQ f r)
+    | isEmpty dq = error "empty deque"
+    | null f = checkfDQ $ DQ f (tail r)
+    | null r = checkfDQ $ DQ (init f) r
+    | otherwise = checkfDQ $ DQ (init f) r
+
+-- TODO: exercise 5.2 prove the bigO is O(1) amortized
+
+
+
 queueTest :: IO ()
 queueTest = do
   quickCheck (prop_head_tail :: BatchedQueue Int -> Bool)
