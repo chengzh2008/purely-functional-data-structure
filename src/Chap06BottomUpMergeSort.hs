7db0ff476c550f0370c31a9bd36d8971969a2f7e
chengzh2008 <chengzh2008@gmail.com>
2018-02-14 09:11:39 -0800

    finish bottomup merge sort implementation

--- src/Chap06BottomUpMergeSort.hs
+++ src/Chap06BottomUpMergeSort.hs
@@ -9,3 +9,27 @@ Bottom-up mergesort first splits a list into n ordered segments (signleton segme
 {-
 The size of all segments are distinct powers of 2, corresponding to the one bits of size n. The individual segments are stored in increasing order of size, and the elements in each segment are stored in increasing order.
 -}
+
+type Size = Int
+data MergeSort a = MS Size [[a]] deriving Show
+
+
+merge :: Ord a => [a] -> [a] -> [a]
+merge [] ys = ys
+merge xs [] = xs
+merge xs'@(x:xs) ys'@(y:ys)
+  | x <= y = x : merge xs ys'
+  | otherwise = y : merge xs' ys
+
+mergeSeg :: Ord a => [a] -> [[a]] -> Size -> [[a]]
+mergeSeg seg segs size
+  | even size = seg : segs
+  -- understanding the size `div` 2
+  | otherwise = mergeSeg (merge seg $ head segs) (tail segs) (size `div` 2)
+
+instance Sortable MergeSort where
+  empty = MS 0 []
+
+  add a ms@(MS size segs) = MS (size + 1) $ mergeSeg [a] segs size
+
+  sort (MS size segs) = foldr (merge) [] segs
--- src/Sortable.hs
+++ src/Sortable.hs
@@ -3,4 +3,4 @@ module Sortable where
 class Sortable s where
   empty :: s a
   add :: Ord a => a -> s a -> s a
-  sort :: Ord a => s a -> s a
+  sort :: Ord a => s a -> [a]
